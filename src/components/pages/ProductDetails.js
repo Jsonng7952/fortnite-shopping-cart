e3476a2fbe8ed920905590507576a7d337b273e1
Jason <jason.ng7952@gmail.com>
2022-05-02 17:20:26 -0400

    Added more items from the featured and special featured in API. Passed data into ProductDetails.js and removed querying by id

--- src/components/pages/ProductDetails.js
+++ src/components/pages/ProductDetails.js
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useParams, useLocation } from 'react-router-dom';
 import '../../css/ProductDetails.css';
 
 function ProductDetails({addProduct}) {
 
-  let {productId, productPrice} = useParams();
-  const [item, setItem] = useState();
+  let {productId} = useParams();
+  const {productName, productDescription, productIcon, productPrice} = useLocation().state;
   const [productCount, setProductCount] = useState(1);
-
-  useEffect(() => {
-    fetch(`https://fortnite-api.com/v2/cosmetics/br/${productId}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.data);
-        setItem(data.data);
-      });
-  }, [productId]);
 
   // When the form is submitted pass the product information to the addProduct method in the RouteSwitch.
   const handleSubmit = (e) => {
     if(productCount > 0) {
-      addProduct(productCount, item.id, item.name, productPrice, item.images.icon);     
+      addProduct(productCount, productId, productName, productPrice, productIcon);     
     }
     e.preventDefault();   
   };
@@ -30,18 +21,18 @@ function ProductDetails({addProduct}) {
     setProductCount(e.target.value);
   };
 
-  return (item === undefined) 
-  ? <div>Loading Product Details...</div> 
-  : <div className='product-detail'>
-      <h1>{item.name}</h1>
-      <div>{item.description}</div>
-      <img src={`${item.images.icon}`} alt='product-icon'></img>
+  return (
+    <div className='product-detail'>
+      <h1>{productName}</h1>
+      <div>{productDescription}</div>
+      <img src={productIcon} alt='product-icon'></img>
       <div>{productPrice}</div>
       <form onSubmit={handleSubmit}>
         <input type='number' min={0} value={productCount} onChange={handleProductCount}></input>
         <button type='submit'>Add to Cart</button>
       </form>
     </div>
+  )
 }
 
 export default ProductDetails;
--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -20,14 +20,116 @@ function Shop() {
       <h1>Daily</h1>
       <div className='daily-shop'>
         {items.daily.entries.map(dailyItem => 
-          <div className='product-card'>
-            <Link to={`/shop/${dailyItem.items[0].id}/${dailyItem.finalPrice}`} className='product-link'>
+          <div key={dailyItem.items[0].id} className='product-card'>
+            <Link className='product-link'
+              to={{
+                pathname:`/shop/${dailyItem.items[0].id}`
+              }}
+              state= {{
+                productName: dailyItem.items[0].name, 
+                productDescription: dailyItem.items[0].description,
+                productIcon: dailyItem.items[0].images.icon,
+                productPrice: dailyItem.finalPrice
+              }}
+            >
               {<img src={`${dailyItem.items[0].images.icon}`} alt='product-card-icon'></img>}
-              <li key={dailyItem.items[0].id}>{dailyItem.items[0].name} : {dailyItem.finalPrice}</li>
+              <li>{dailyItem.items[0].name} : {dailyItem.finalPrice}</li>
             </Link>
           </div>
         )}        
       </div>
+      <h1>Featured</h1>
+      <div className='featured-shop'>
+        {items.featured.entries.map(featuredItem => {
+          // Check to see if item is a bundle or not.
+          if(featuredItem.bundle === null) {
+            return (
+              <div key={featuredItem.items[0].id} className='product-card'>
+                <Link className='product-link'
+                  to={{
+                    pathname: `/shop/${featuredItem.items[0].id}`
+                  }} 
+                  state= {{
+                    productName: featuredItem.items[0].name, 
+                    productDescription: featuredItem.items[0].description,
+                    productIcon: featuredItem.items[0].images.icon,
+                    productPrice: featuredItem.finalPrice
+                  }}                  
+                >
+                  {<img src={`${featuredItem.items[0].images.icon}`} alt='product-card-icon'></img>}
+                  <li>{featuredItem.items[0].name} : {featuredItem.finalPrice}</li>
+                </Link>
+              </div>                 
+            )
+          }
+          else {
+            return (
+              <div key={featuredItem.bundle.name.split(' ').join('_')} className='product-card'>
+                <Link className='product-link'
+                  to={{
+                    pathname: `/shop/${featuredItem.bundle.name.split(' ').join('_')}`
+                  }} 
+                  state= {{
+                    productName: featuredItem.bundle.name, 
+                    productDescription: featuredItem.bundle.info,
+                    productIcon: featuredItem.bundle.image,
+                    productPrice: featuredItem.finalPrice
+                  }}                        
+                >
+                  {<img src={`${featuredItem.bundle.image}`} alt='product-card-icon'></img>}
+                  <li>{featuredItem.bundle.name} : {featuredItem.finalPrice}</li>
+                </Link>
+              </div>                   
+            )
+          }
+        })}
+      </div>
+      <h1>Special Featured</h1>
+      <div className='special-featured-shop'>
+        {items.specialFeatured.entries.map(specialFeaturedItem => {
+          // Check to see if item is a bundle or not.          
+          if(specialFeaturedItem.bundle === null) {
+            return (
+              <div key={specialFeaturedItem.items[0].id} className='product-card'>
+                <Link className='product-link'
+                  to={{
+                    pathname: `/shop/${specialFeaturedItem.items[0].id}`
+                  }} 
+                  state= {{
+                    productName: specialFeaturedItem.items[0].name, 
+                    productDescription: specialFeaturedItem.items[0].description,
+                    productIcon: specialFeaturedItem.items[0].images.icon,
+                    productPrice: specialFeaturedItem.finalPrice
+                  }}                   
+                >
+                  {<img src={`${specialFeaturedItem.items[0].images.icon}`} alt='product-card-icon'></img>}
+                  <li>{specialFeaturedItem.items[0].name} : {specialFeaturedItem.finalPrice}</li>
+                </Link>
+              </div>                 
+            )
+          }
+          else {
+            return (
+              <div key={specialFeaturedItem.bundle.name.split(' ').join('_')} className='product-card'>
+                <Link className='product-link'
+                  to={{
+                    pathname: `/shop/${specialFeaturedItem.bundle.name.split(' ').join('_')}`
+                  }} 
+                  state= {{
+                    productName: specialFeaturedItem.bundle.name, 
+                    productDescription: specialFeaturedItem.bundle.info,
+                    productIcon: specialFeaturedItem.bundle.image,
+                    productPrice: specialFeaturedItem.finalPrice
+                  }}                     
+                >
+                  {<img src={`${specialFeaturedItem.bundle.image}`} alt='product-card-icon'></img>}
+                  <li>{specialFeaturedItem.bundle.name} : {specialFeaturedItem.finalPrice}</li>
+                </Link>
+              </div>                   
+            )
+          }
+        })}
+      </div>      
     </div>
     
 
