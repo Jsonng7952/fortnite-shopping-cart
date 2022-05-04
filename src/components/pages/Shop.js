ac69b0c1b51621b224aaf3defd34abe3ee4386f6
Jason <jason.ng7952@gmail.com>
2022-05-04 17:43:33 -0400

    Update filter to include bundles and changed images to include background

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -24,7 +24,7 @@ function Shop() {
   const displayShop = (shopData) => {
     // Check to see if the data is a bundle item or not. 
     return (shopData.bundle === null)
-    ? <div key={shopData.items[0].id} className='product-card'>
+    ? <div key={shopData.items[0].id} className={`product-card ${shopData.items[0].rarity.value}`}>
         <Link className='product-link'
           to={{
             pathname:`/shop/${shopData.items[0].id}`
@@ -36,9 +36,13 @@ function Shop() {
             productPrice: shopData.finalPrice
           }}
         >
-          {<img src={`${shopData.items[0].images.featured}`} alt='product-card-icon'></img>}
-          <div>{shopData.items[0].name} : {shopData.finalPrice}</div>
-          <div>{shopData.items[0].type.value} : {shopData.items[0].rarity.value}</div>
+          {<img src={`${shopData.newDisplayAsset.materialInstances[0].images.Background}`} alt='product-card-icon'></img>}
+          <div className='product-shop-info'>
+            <div>{shopData.items[0].name.toUpperCase()}</div>
+            <div><img src={items.vbuckIcon} alt='vbuck-icon' className='vbuck-icon'></img>{shopData.finalPrice}</div>
+          </div>
+          
+          {/*<div>{shopData.items[0].type.value} : {shopData.items[0].rarity.value}</div>*/}
         </Link>
       </div>    
     : <div key={shopData.bundle.name.split(' ').join('_')} className='product-card'>
@@ -53,8 +57,11 @@ function Shop() {
             productPrice: shopData.finalPrice
           }}                        
         >
-          {<img src={`${shopData.bundle.image}`} alt='product-card-icon'></img>}
-          <div>{shopData.bundle.name} : {shopData.finalPrice}</div>
+          {<img src={`${shopData.newDisplayAsset.materialInstances[0].images.Background}`} alt='product-card-icon'></img>}
+          <div className='product-shop-info'>
+            <div>{shopData.bundle.name.toUpperCase()}</div>
+            <div><img src={items.vbuckIcon} alt='vbuck-icon' className='vbuck-icon'></img>{shopData.finalPrice}</div>
+          </div>
         </Link>
       </div>
   };
@@ -63,7 +70,7 @@ function Shop() {
     <div>Loading Data...</div> :
     <div className='shop'>
       <Filter filterResults={filterResults}/>
-      <h1>Daily</h1>
+      <h1 className='shop-header'>Daily</h1>
       <div className='daily-shop'>
         {(filteredResults === 'all' || filteredResults === '') 
           ? items.daily.entries.map(dailyItem => displayShop(dailyItem))  
@@ -72,21 +79,23 @@ function Shop() {
             .map(dailyItem => displayShop(dailyItem))
         }
       </div>
-      <h1>Featured</h1>
+      <h1 className='shop-header'>Featured</h1>
       <div className='featured-shop'>
         {(filteredResults === 'all' || filteredResults === '') 
           ? items.featured.entries.map(featuredItem => displayShop(featuredItem))  
           : items.featured.entries
-            .filter(featuredItem => featuredItem.items[0].type.value === filteredResults)
+            .filter(featuredItem => featuredItem.items[0].type.value === filteredResults 
+              || (featuredItem.bundle !== null && featuredItem.items.some(item => item.type.value === filteredResults)))
             .map(featuredItem => displayShop(featuredItem))
         }
       </div>
-      <h1>Special Featured</h1>
+      <h1 className='shop-header'>Special Featured</h1>
       <div className='special-featured-shop'>
         {(filteredResults === 'all' || filteredResults === '') 
           ? items.specialFeatured.entries.map(specialFeatured => displayShop(specialFeatured))  
           : items.specialFeatured.entries
-            .filter(specialFeatured => specialFeatured.items[0].type.value === filteredResults)
+            .filter(specialFeatured => specialFeatured.items[0].type.value === filteredResults 
+              || (specialFeatured.bundle !== null && specialFeatured.items.some(item => item.type.value === filteredResults)))
             .map(specialFeatured => displayShop(specialFeatured))
         }
       </div>      
