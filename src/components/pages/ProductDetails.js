e51cf354bfe11bb622b73327dafb1c0398361c33
Jason <jason.ng7952@gmail.com>
2022-05-06 17:21:39 -0400

    Added className and added containers for styling

--- src/components/pages/ProductDetails.js
+++ src/components/pages/ProductDetails.js
@@ -5,10 +5,11 @@ import '../../css/ProductDetails.css';
 function ProductDetails({addProduct}) {
 
   let {productId} = useParams();
-  const {productName, productDescription, productIcon, productPrice} = useLocation().state;
+  const {productName, productDescription, productIcon, productPrice, vBuckIcon, productRarity} = useLocation().state;
   const [productCount, setProductCount] = useState(1);
 
   // When the form is submitted pass the product information to the addProduct method in the RouteSwitch.
+  // Adds the product into the cart.
   const handleSubmit = (e) => {
     if(productCount > 0) {
       addProduct(productCount, productId, productName, productPrice, productIcon);     
@@ -23,14 +24,18 @@ function ProductDetails({addProduct}) {
 
   return (
     <div className='product-detail'>
-      <h1>{productName}</h1>
-      <div>{productDescription}</div>
-      <img src={productIcon} alt='product-icon'></img>
-      <div>{productPrice}</div>
-      <form onSubmit={handleSubmit}>
-        <input type='number' min={0} value={productCount} onChange={handleProductCount}></input>
-        <button type='submit'>Add to Cart</button>
-      </form>
+      <div className='product-info-header'>
+        <div className='product-name'>{productName}</div>
+        <div className='product-description'>{productDescription}</div>
+        <img className={`product-icon ${productRarity}`} src={productIcon} alt='product-icon'></img>        
+      </div>
+      <div className='product-info-bottom'>
+        <div className='product-price'><img src={vBuckIcon} alt='vbuck-icon' className='vbuck-icon'></img>{productPrice}</div>
+        <form onSubmit={handleSubmit} className='product-form'>
+          <input type='number' min={0} value={productCount} onChange={handleProductCount}></input>
+          <button type='submit'>Add to Cart</button>
+        </form>        
+      </div>
     </div>
   )
 }
